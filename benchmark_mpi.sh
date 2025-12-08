c4ecb1866af32c6c07de58a2996fb40ac74dc29f
Moore <cmoore12@node8.cl.luc.edu>
2025-12-08 02:56:53 -0600

    quick change to bash file

--- benchmark_mpi.sh
+++ benchmark_mpi.sh
@@ -6,7 +6,7 @@ OUTPUT_FILE="benchmark_results_mpi.csv"
 # Benchmark Parameters
 STEPS=1000
 SIZES="500 1000 2000 5000 10000 20000"
-PROCS="1 2 4 8 16"
+PROCS="32 64"
 
 # Run MPI Benchmark
 echo "Running MPI Benchmarks..."
@@ -14,7 +14,7 @@ for p in $PROCS; do
     echo "  - Processes: $p"
     for s in $SIZES; do
         echo "      - Grid: $s x $s"
-        mpirun $MPI_ARGS -np $p ./sir_sim_mpi $STEPS $s $s | tee /dev/tty | grep "CSV_DATA" | sed "s/CSV_DATA,/MPI,$p,/" >> $OUTPUT_FILE
+        mpirun $MPI_ARGS -np $p --use-hwthread-cpus ./sir_sim_mpi $STEPS $s $s | tee /dev/tty | grep "CSV_DATA" | sed "s/CSV_DATA,/MPI,$p,/" >> $OUTPUT_FILE
     done
 done
 
